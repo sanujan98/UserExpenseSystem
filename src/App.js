a639bd51ddc3d9ee3e6aa654336725c92a10ee27
Sanujan <61859286+sanujan98@users.noreply.github.com>
2023-07-06 15:37:55 +0530

    Added register,login and dashboard pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+import Dashboard from './Screens/dashboard';
+import Login from './Screens/login';
+import Register from './Screens/register';
+import forgetpassword from './Screens/forgetpassword';
+// ...import other page components
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        
+          
+          <Route path="login" element={<Login/>} />
+          <Route path="register" element={<Register />} />
+          <Route path="/" element={<Dashboard />} />
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+
+
+
